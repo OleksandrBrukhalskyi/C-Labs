2979efb30267a8eef4a661caaab702ccde7f88b3
Oleksandr Brukhalskyi <46578065+OleksandrBrukhalskyi@users.noreply.github.com>
2020-05-31 14:54:23 +0300

    Update task6.1.cpp

--- C++ Labs/tema6/task6.1.cpp
+++ C++ Labs/tema6/task6.1.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+using namespace std;
+double SumRange(int a, int b) {
+	   double sum=0;
+	   if(a>=b){
+	     return 0;
+	   }
+	   else{
+	     for(int i=a; i<=b;i++){
+	       sum+=i;
+	     }
+	     return sum;
+	   }
+	 }
+int main() {
+	double res = SumRange(2, 5);
+	 cout<<"Result:"<<res;
+}
