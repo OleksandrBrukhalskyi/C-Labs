d5471d049cfe5ac0dad78aff2d9f0d1f39ca6c6d
Oleksandr Brukhalskyi <46578065+OleksandrBrukhalskyi@users.noreply.github.com>
2020-05-31 14:45:27 +0300

    Update task8.2.cpp

--- C++ Labs/tema8/task8.2.cpp
+++ C++ Labs/tema8/task8.2.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int factorial(int x) {
+	if (x <= 1) {
+		return 1;
+	}
+	else {
+		return x * factorial(x - 1);
+	}
+}
+void buildTeamWithJumper() {
+	int res =  factorial(6) / (factorial(3) * factorial(3))
+		* factorial(3) / (factorial(2) * factorial(1)) + factorial(6) / (factorial(4) * factorial(2)) * factorial(3) / (factorial(1) * factorial(2)) +
+		factorial(6) / (factorial(5) * factorial(1)) * factorial(1);
+		cout<<res;
+}
+int main() {
+	buildTeamWithJumper();
+}
