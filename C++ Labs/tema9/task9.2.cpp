5dff691ccfa5a04a5951f997de40135fb7eecc33
Oleksandr Brukhalskyi <46578065+OleksandrBrukhalskyi@users.noreply.github.com>
2020-06-01 10:38:02 +0300

    Create task9.2.cpp

---
+++ C++ Labs/tema9/task9.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+string longestWord(string str) {
+	string tmp = "";
+	string maxWord = "";
+	for (int i = 0; i < str.size(); ++i) {
+		if (str[i] != ' ') {
+			tmp += str[i];
+		}
+		else {
+			tmp = "";
+		}
+		if (tmp.size() > maxWord.size()) {
+			maxWord = tmp;
+		}
+	}
+	return maxWord;
+}
+int main() {
+	string str;
+	cout << "Input string:" << endl;
+	cin >> str;
+	cout << "The longest word is " << longestWord(str) << endl;
+}
